83a4d52d54170a4215832d3124b791e84e65f980
rednaks69 <hasnaoui.azer@gmail.com>
2026-06-15 09:41:14 +0100

    assets, metaData, fonts, globalcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="text-3xl font-bold">yo</div>;
+  return (
+    <div>
+      <h1 className="">yo</h1>
+    </div>
+  );
 };
 
 export default Home;
